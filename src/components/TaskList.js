4c2ead3db85e1081e73a739560c43eb2f01d7a34
John Truong <viajohnt@gmail.com>
2023-04-16 18:59:41 -0600

    Final Commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Task from "./Task"
+import { v4 as uuidv4} from "uuid"
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({tasks, onDeleteTask}) {
+  const myTasks = tasks.map ((task) => 
+    <Task 
+      key = {uuidv4()}
+      text={task.text}
+      category = {task.category}
+      onDeleteTask={onDeleteTask}
+      />
+    );
+     return <div className="tasks">{myTasks}</div>;
 }
 
 export default TaskList;
+
